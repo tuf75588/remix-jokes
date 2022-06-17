9a0a474e67fff840a3a50b7eb5fd351564b5efd4
Andrew Davis <atd285@gmail.com>
2022-06-17 12:32:16 -0400

    render appropriate jokes based on unique ID

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,31 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData, Link } from '@remix-run/react';
+import { db } from '~/utils/db.server';
+import type {Joke} from '@prisma/client';
+
+type LoaderData = {
+  joke: Joke;
+}
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { jokeId } = params;
+  console.log(jokeId);
+  const joke = await db.joke.findUnique({
+    where: { id: jokeId },
+  });
+  if (!joke) throw new Error('joke not found!');
+  const data: LoaderData = {joke};
+  return json(data);
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>Why don't you find hippopotamuses hiding in trees? They're really good at it.</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
