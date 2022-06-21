b1b5ec193f00514dfc5acfe1d4df026bd20d13e6
Andrew Davis <atd285@gmail.com>
2022-06-21 12:02:40 -0400

    polish new jokes route a bit

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,32 +1,38 @@
-import { redirect } from "@remix-run/node";
+import { redirect } from '@remix-run/node';
+import { db } from '~/utils/db.server';
+import type { ActionFunction } from '@remix-run/node';
 
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get('name');
+  const content = form.get('content');
 
-export async function action({request}: any) {
-  const body = await request.formData();
-  const inputName = body.get('name');
-  const inputContent = body.get('content');
-  console.log(inputName, inputContent);
-  return redirect('/jokes')
-}
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error('Something went wrong.  Form not submitted correctly');
+  }
+  const fields = { name, content };
 
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
 
 export default function NewJokeRoute() {
   return (
     <div>
       <p>Add your own hilarious joke</p>
-      <form method="post">
+      <form method='post'>
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name: <input type='text' name='name' />
           </label>
         </div>
         <div>
           <label>
-            Content: <textarea name="content" />
+            Content: <textarea name='content' />
           </label>
         </div>
         <div>
-          <button type="submit" className="button">
+          <button type='submit' className='button'>
             Add
           </button>
         </div>
