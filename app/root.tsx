6d57452fe8833619cd1560cc2318d2d2a2ab32f2
Andrew Davis <atd285@gmail.com>
2022-06-30 15:06:09 -0400

    add meta function to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { Links, LiveReload, Outlet, useCatch } from '@remix-run/react';
-import type { LinksFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, useCatch } from '@remix-run/react';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 
 import globalStylesUrl from './styles/global.css';
 import globalMediumStylesUrl from './styles/global-medium.css';
@@ -25,6 +25,21 @@ export const links: LinksFunction = () => {
   ];
 };
 
+export const meta: MetaFunction = () => {
+  const description = `Learn Remix!`;
+  return {
+    charSet: 'utf-8',
+    description,
+    keywords: 'Remix,jokes',
+    'twitter:image': 'https://remix-jokes/lol/social.png',
+    'twitter:card': 'summary_large_image',
+    'twitter:creator': '@remix_run',
+    'twitter:site': '@remix_run',
+    'twitter:title': 'Remix Jokes',
+    'twitter:description': description,
+  };
+};
+
 function Document({
   children,
   title = `Remix Jokes!`,
@@ -35,7 +50,7 @@ function Document({
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
+        <Meta />
         <title>{title}</title>
         <Links />
       </head>
